885be7782701e17543df198579d1dca06005d0da
mosjikung <mosjikung@gmail.com>
2021-04-09 00:07:32 +0700

    new commit

--- it/app_fix.php
+++ it/app_fix.php
@@ -36,7 +36,7 @@ $result2 = $showapp->showlistapp($fix_stat,$can_fix,$can_fix2,$mgr_app);
   <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
   <meta name="description" content="Start your development with a Dashboard for Bootstrap 4.">
   <meta name="author" content="Creative Tim">
-  <title>Argon Dashboard - Free Dashboard for Bootstrap 4</title>
+  <title>Request Fix Nutritionsc</title>
   <!-- Favicon -->
   <link rel="icon" href="assets/img/brand/favicon.png" type="image/png">
   <!-- Fonts -->
