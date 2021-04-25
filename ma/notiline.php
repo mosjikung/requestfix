589121ac3b4a9bc9bd29922904f67e022c00b58f
mosjikung <mosjikung@gmail.com>
2021-04-25 18:56:01 +0700

    new commit

---
+++ ma/notiline.php
@@ -0,0 +1,32 @@
+<?php
+$str = "แจ้งซ่อม ="."From : $username
+แจ้งซ่อมเกี่ยวกับ : $about ฝ่าย : $section 
+อาการ : $problem
+สถานที่ : $address;
+วันที่ยื่น : $crt_time";
+$chOne = curl_init();
+curl_setopt( $chOne, CURLOPT_URL, "https://notify-api.line.me/api/notify");
+// SSL USE
+curl_setopt( $chOne, CURLOPT_SSL_VERIFYHOST, 0);
+curl_setopt( $chOne, CURLOPT_SSL_VERIFYPEER, 0);
+//POST
+curl_setopt( $chOne, CURLOPT_POST, 1);
+// Message
+curl_setopt( $chOne, CURLOPT_POSTFIELDS, "message=ระบบอัตโนมัติ.$str");
+//ถ้าต้องการใส่รุป ให้ใส่ 2 parameter imageThumbnail และimageFullsize
+//curl_setopt( $chOne, CURLOPT_POSTFIELDS, "message=hi&imageThumbnail=http://www.wisadev.com/wp-content/uploads/2016/08/cropped-wisadevLogo.png&imageFullsize=http://www.wisadev.com/wp-content/uploads/2016/08/cropped-wisadevLogo.png");
+// follow redirects
+curl_setopt( $chOne, CURLOPT_FOLLOWLOCATION, 1);
+//ADD header array
+$headers = array( 'Content-type: application/x-www-form-urlencoded', 'Authorization: Bearer WFlsVAMiuh5XMzcafFbocLEhmk3ATrI04Pxd5RhvrPP', );
+curl_setopt($chOne, CURLOPT_HTTPHEADER, $headers);
+//RETURN
+curl_setopt( $chOne, CURLOPT_RETURNTRANSFER, 1);
+$result = curl_exec( $chOne );
+//Check error
+if(curl_error($chOne)) { echo 'error:' . curl_error($chOne); }
+else { $result_ = json_decode($result, true);
+echo "status : ".$result_['status']; echo "message : ". $result_['message']; }
+//Close connect
+curl_close( $chOne );
+?>
